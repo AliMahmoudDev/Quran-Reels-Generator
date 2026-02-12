dcf46a8449c4f207aeacb07fedb37612465c2a4d
Ali Mahmmoud <127209252+AliMahmoudDev@users.noreply.github.com>
2026-02-12 12:11:43 +0200

    Update port to 5000

--- main.py
+++ main.py
@@ -410,8 +410,9 @@ def conf(): return jsonify({'surahs': SURAH_NAMES, 'verseCounts': VERSE_COUNTS, 
 def out(f): return send_from_directory(TEMP_DIR, f)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
 
 
 
