0b868baefdad59a33940ffe10d900b929c1cdbb4
Ali Mahmmoud <127209252+AliMahmoudDev@users.noreply.github.com>
2026-02-12 11:54:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -411,6 +411,7 @@ def out(f): return send_from_directory(TEMP_DIR, f)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
+
 
 
