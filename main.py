d91fe3048b03bcf6fceefc4e66613f85582095dd
Ali Mahmmoud <127209252+AliMahmoudDev@users.noreply.github.com>
2026-02-14 22:46:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -329,7 +329,7 @@ def build_video(user_pexels_key, reciter_id, surah, start, end=None, quality='72
         out = os.path.join(TEMP_DIR, fname) 
         add_log('Rendering Final Video...')
         my_logger = QuranLogger()
-        final.write_videofile(out, fps=15, codec='libx264', audio_bitrate='96k', preset='ultrafast', threads=1, verbose=False, logger=my_logger, ffmpeg_params=['-movflags', '+faststart', '-pix_fmt', 'yuv420p', '-crf', '28'])
+        final.write_videofile(out, fps=15, codec='libx264', audio_bitrate='96k', preset='ultrafast', threads=4, verbose=False, logger=my_logger, ffmpeg_params=['-movflags', '+faststart', '-pix_fmt', 'yuv420p', '-crf', '28'])
         
         update_progress(100, 'Done!')
         current_progress['is_complete'] = True 
@@ -388,4 +388,5 @@ def out(f): return send_from_directory(TEMP_DIR, f)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
 
