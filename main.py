d4c68cda712c0bd969ee91ae6e1367269b18dfc9
Ali Mahmmoud <127209252+AliMahmoudDev@users.noreply.github.com>
2026-03-10 22:05:00 +0200

    Change greeting from 'Hello World' to 'Goodbye World'

--- main.py
+++ main.py
@@ -234,14 +234,14 @@ def process_mp3quran_audio(reciter_name, surah, ayah, idx, workspace_dir, job_id
     duration = len(seg)
     
     # 🚀 التعديل السحري: إضافة مقص إجباري في البداية لقتل "التسريب"
-    aggressive_start_trim = start_trim + 300  # قص 150 ملي ثانية إضافية من أول الآية
+    aggressive_start_trim = max(0,start_trim- 30)  # قص 150 ملي ثانية إضافية من أول الآية
     
     # ترك مساحة أمان في النهاية للحفاظ على صدى الشيخ
     # 🧪 تجربة البتر: هنجبره يقص 300 ملي ثانية زيادة من آخر الآية عشان نكتشف مصدر الصوت!
-    experiment_cut = 250 
+    experiment_cut = 400
     
     # لاحظ خلينا الـ end_trim يزيد عليه 300 ملي ثانية عشان ياكل من آخر الصوت
-    safe_end_trim = end_trim + experiment_cut 
+    safe_end_trim = max(0,end_trim-experiment_cut )
     
     if duration - start_trim - safe_end_trim > 200: 
         seg = seg[start_trim:duration-safe_end_trim]
@@ -651,4 +651,5 @@ threading.Thread(target=background_cleanup, daemon=True).start()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=7860, threaded=True)
+
 
